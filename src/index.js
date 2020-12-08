25b16ecf0e83be48fe7e00a118743520f53a449b
Łukasz Bień <68947712+lukasz-bien-rmtcfm-com@users.noreply.github.com>
2020-12-08 20:33:22 +0100

    Redis rate limiting (#1)
    * Redis rate limiting
    
    * Fixes
    
    * Fixes 2
    
    * Fixes 3
    
    * Fixes 4

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const redis = require('redis');
+const RateLimit = require('express-rate-limit');
+const RedisStore = require('rate-limit-redis');
+const { promisify } = require('util');
+
+require('dotenv').config();
+
+const app = express();
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
+
+redisClient.get = promisify(redisClient.get);
+redisClient.set = promisify(redisClient.set);
+
+app.use(bodyParser.json());
+
+const limiter = async (req, res, next) => {
+    const limit = await redisClient.get('ratelimit');
+
+    const limiter = new RateLimit({
+        store: new RedisStore({
+            client: redisClient,
+            expiry: 10
+        }),
+        max: limit,
+        windowMs: 10 * 1000
+    });
+
+    return limiter(req, res, next);
+};
+
+app.use('/', express.static(path.join(__dirname, './public')));
+
+app.get('/api/ping', limiter, (req, res) => {
+    return res.send('PONG');
+});
+
+app.put('/api/set-limit', async (req, res) => {
+    const { limit } = req.body;
+
+    await redisClient.set('ratelimit', parseInt(limit));
+
+    return res.send({ limit });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`APP is listening on port: ${process.env.PORT}`);
+});
---
+++ src/public/script.js
@@ -0,0 +1,134 @@
+const sendButton = document.querySelector('#test-button');
+const resetButton = document.querySelector('#reset-button');
+const limitSelect = document.querySelector('#limit-select');
+const timerDiv = document.querySelector('#timer');
+const resultDiv = document.querySelector('#result');
+const pingUrl = '/api/ping';
+const setLimitUrl = '/api/set-limit';
+let responses = {
+    successful: 0,
+    limited: 0,
+    unknown: 0
+};
+let timeout;
+
+const onLimitSelectChange = async e => {
+    try {
+        await axios.put(setLimitUrl, { limit: e.target.value });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const onSendButtonClick = async e => {
+    e.preventDefault();
+
+    if (!timeout) {
+        let interval;
+        let counter = 10;
+
+        limitSelect.disabled = true;
+
+        timeout = setTimeout(() => {
+            resetButton.disabled = false;
+            sendButton.disabled = true;
+            limitSelect.disabled = false;
+            clearInterval(interval);
+            timerDiv.innerHTML = 'Done, reset your stats';
+        }, 10 * 1000);
+
+        timerDiv.innerHTML = `${counter} seconds left`;
+
+        interval = setInterval(() => {
+            counter--;
+            timerDiv.innerHTML = `${counter} seconds left`;
+        }, 1000);
+    }
+
+    let results = [];
+    let calls = [];
+
+    const callPing = async () => {
+        try {
+            const response = await axios.get(pingUrl);
+
+            results.push(response);
+        } catch (err) {
+            results.push(err.response);
+        }
+    };
+
+    for (let i = 0; i < 5; i++) {
+        calls.push(callPing());
+    }
+
+    await Promise.all(calls);
+
+    results.forEach(result => {
+        if (!result || !result.status || (result.status !== 200 && result.status !== 429)) {
+            responses.unknown++;
+        }
+        if (result.status === 200) {
+            responses.successful++;
+        }
+        if (result.status === 429) {
+            responses.limited++;
+        }
+    });
+
+    resultDiv.innerHTML = '';
+
+    if (responses.successful) {
+        const successfullMessage = document.createElement('h3');
+
+        successfullMessage.style.color = 'green';
+        successfullMessage.innerHTML = `${responses.successful} requests were handled properly (status 200)`;
+        resultDiv.appendChild(successfullMessage);
+    }
+
+    if (responses.limited) {
+        const limitedMessage = document.createElement('h3');
+
+        limitedMessage.style.color = 'red';
+        limitedMessage.innerHTML = `${responses.limited} requests have been blocked by rate limit (status 429)`;
+        resultDiv.appendChild(limitedMessage);
+    }
+
+    if (responses.unknown) {
+        const unknownMessage = document.createElement('h3');
+
+        unknownMessage.style.color = 'red';
+        unknownMessage.innerHTML = `${responses.unknown} requests had server error (different status)`;
+        resultDiv.appendChild(unknownMessage);
+    }
+};
+
+const onResetButtonClick = e => {
+    e.preventDefault();
+    responses.successful = 0;
+    responses.limited = 0;
+    responses.unknown = 0;
+    sendButton.disabled = false;
+    resultDiv.innerHTML = '';
+};
+
+document.addEventListener(
+    'DOMContentLoaded',
+    async () => {
+        resetButton.disabled = true;
+        limitSelect.value = 5;
+
+        try {
+            await axios.put(setLimitUrl, { limit: 5 });
+        } catch (err) {
+            console.error(err);
+        }
+    },
+    false
+);
+
+limitSelect.addEventListener('change', onLimitSelectChange);
+
+sendButton.addEventListener('click', onSendButtonClick);
+
+resetButton.addEventListener('click', onResetButtonClick);
